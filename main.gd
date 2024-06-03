63624ca9873d8dacfb4479f1c2be1e1d285e0ae8
poiuygfd <jamphillips2000@gmail.com>
2024-06-03 23:09:28 +0100

    yellow submarine

--- main.gd
+++ main.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 
 func _ready() -> void:
-	get_tree().change_scene_to_file("res://characters/beet/beet_arena.tscn")
-	#get_tree().change_scene_to_file("res://characters/lemon/lemon_stage.tscn")
+	#get_tree().change_scene_to_file("res://characters/beet/beet_arena.tscn")
+	get_tree().change_scene_to_file("res://characters/lemon/lemon_stage.tscn")
 
 
 
