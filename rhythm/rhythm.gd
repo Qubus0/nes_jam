f7178aaac255d8e29aefc9d8cc61aee3cce51b1a
Qubus0 <steen.rickmer@gmx.de>
2024-06-05 00:19:15 +0200

    rhythm precision

--- rhythm/rhythm.gd
+++ rhythm/rhythm.gd
@@ -1,8 +1,12 @@
 extends Control
 
 var beat_speed := 40
-
-const BEAT = preload("res://rhythm/beat.tscn")
+enum {
+	PERFECT,
+	SOLID,
+	WEAK,
+	MISS
+}
 
 @onready var valid := %ValidArea
 var playhead: PathFollow2D
@@ -34,13 +38,14 @@ func playhead_entered(area: Area2D) -> void:
 	var beat := area.get_parent()
 	var new_beat: Beat = beat.duplicate()
 
+	await get_tree().process_frame # gets rid of the error
 	$Track.add_child(new_beat)
 	new_beat.speed = beat_speed
 	new_beat.progress_ratio = 0
 
 
 func handle_input() -> void:
-	if not valid.has_overlapping_areas():
+	if valid.get_overlapping_areas().is_empty():
 		return
 
 	var detected_area: Area2D = valid.get_overlapping_areas().front()
@@ -48,23 +53,40 @@ func handle_input() -> void:
 		return
 	var beat: Beat = detected_area.get_parent()
 
-	$Label.text = ""
 	if Input.is_action_just_pressed("A") or Input.is_action_just_pressed("B"):
 		var direction := -1 if beat.face_left else 1
 		if not Input.get_axis("B", "A") == direction:
-			$Label.text = "miss!"
-			return
-
-		if %Perfect.has_overlapping_areas():
-			$Label.text = "Perfect!"
+			accuracy(MISS)
+		elif %Perfect.has_overlapping_areas():
+			accuracy(PERFECT)
 		elif %Solid.has_overlapping_areas():
-			$Label.text = "solid"
+			accuracy(SOLID)
 		elif %Weak.has_overlapping_areas():
-			$Label.text = "weak"
+			accuracy(WEAK)
 		else:
-			$Label.text = "miss!"
+			accuracy(MISS)
+
+		beat.get_parent().remove_child(beat)
+		beat.queue_free()
 
 
 func _on_miss_area_entered(area: Area2D) -> void:
-	$Label.text = "miss!"
+	accuracy(MISS)
 	area.get_parent().queue_free()
+
+
+func accuracy(level: int) -> void:
+	print(level)
+	var new_text: Sprite2D = $AccuracyText.duplicate()
+	add_child(new_text)
+	new_text.frame = level
+	new_text.show()
+
+	var tw := get_tree().create_tween()
+
+	tw.tween_property(new_text, "position:y", -30, 0.7).as_relative()
+	tw.parallel().tween_property(new_text, "modulate:a", 0, 0.7)
+	tw.finished.connect(
+		func(): if new_text: new_text.queue_free()
+	)
+
