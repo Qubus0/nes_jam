2e268c22b6f12e50d975b6ef3a55406215fdf866
Qubus0 <steen.rickmer@gmx.de>
2024-06-05 00:19:22 +0200

    main menu

--- main.gd
+++ main.gd
@@ -2,8 +2,12 @@ extends Node2D
 
 
 func _ready() -> void:
-	#get_tree().change_scene_to_file("res://characters/beet/beet_arena.tscn")
+	pass
+
+
+func _on_lemon_pressed() -> void:
 	get_tree().change_scene_to_file("res://characters/lemon/lemon_stage.tscn")
 
 
-
+func _on_beet_pressed() -> void:
+	get_tree().change_scene_to_file("res://characters/beet/beet_arena.tscn")
