cbb8e44f60ad044564a8db8c667d562cca998f8c
Qubus0 <steen.rickmer@gmx.de>
2024-06-15 22:50:54 +0200

    cherry map fixes

--- characters/cherry/cherry_stage.gd
+++ characters/cherry/cherry_stage.gd
@@ -4,9 +4,9 @@ extends Node2D
 
 func _process(delta: float) -> void:
 	$Player/Camera2D.drag_horizontal_offset = remap(
-			clampf($Player.velocity.x, -300, 300),
-			-300, 300, -1, 1
-		)
+		clampf($Player.velocity.x, -300, 300),
+		-300, 300, -1, 1
+	)
 
 
 func _on_out_of_bounds_body_entered(_body: Node2D) -> void:
