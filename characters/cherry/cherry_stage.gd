5924e4816a7e4fa2ba739e52e07de9c9040848d4
Qubus0 <steen.rickmer@gmx.de>
2024-06-15 22:41:09 +0200

    cherry cam lookahead

--- characters/cherry/cherry_stage.gd
+++ characters/cherry/cherry_stage.gd
@@ -2,6 +2,13 @@ extends Node2D
 
 
 
+func _process(delta: float) -> void:
+	$Player/Camera2D.drag_horizontal_offset = remap(
+			clampf($Player.velocity.x, -300, 300),
+			-300, 300, -1, 1
+		)
+
+
 func _on_out_of_bounds_body_entered(_body: Node2D) -> void:
 	Global.game_over(self)
 
