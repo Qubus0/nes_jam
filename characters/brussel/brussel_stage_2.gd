a63f63c47b13509a541ffc38ac0ed670425ae17a
Qubus0 <steen.rickmer@gmx.de>
2024-06-17 12:35:59 +0200

    brussel 2 to 3 dia

--- characters/brussel/brussel_stage_2.gd
+++ characters/brussel/brussel_stage_2.gd
@@ -14,4 +14,5 @@ func _process(delta: float) -> void:
 		drum_beat = true
 
 func _on_phase_transition_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_packed(STAGE_3)
+	Global.dialogue(Global.conversation.BRUSSEL_STAGE3_START)
+
