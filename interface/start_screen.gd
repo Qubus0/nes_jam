3340be4d758eae7fd6d29110788488d31559e160
Qubus0 <steen.rickmer@gmx.de>
2024-06-17 02:33:54 +0200

    start screen

---
+++ interface/start_screen.gd
@@ -0,0 +1,7 @@
+extends Control
+
+
+
+func _unhandled_key_input(event: InputEvent) -> void:
+	if event.is_action_pressed(&"Start") or event.is_action_pressed(&"Select"):
+		Global.dialogue(Global.conversation.INTRO_START)
